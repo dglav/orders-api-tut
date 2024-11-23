d8191e0947e4a24950a2dad89f9777c3d268a77a
Drew Glavich <drewglavich@Drews-MacBook-Pro-2.local>
2024-11-24 00:28:45 +0900

    feat: refactor routes into application folder and create order handlers

---
+++ application/app.go
@@ -0,0 +1,34 @@
+// To pick up where I left off: https://www.youtube.com/watch?v=PWukxD1DC0I&list=PL4cUxeGkcC9iImF8w9FbFOc2UntutL9Wv&index=3
+package application
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type App struct {
+	router http.Handler
+}
+
+func New() *App {
+	app := &App{
+		router: loadRoutes(),
+	}
+
+	return app
+}
+
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+
+	return nil
+}
